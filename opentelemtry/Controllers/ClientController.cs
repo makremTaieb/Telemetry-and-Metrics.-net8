eed95ca76198758adfa8319a2be89c354a4121c5
Montassar Taieb <makrem16790@gmail.com>
2025-03-17 23:40:09 +0100

    add client controller add logging add docker compose & adding the service aspire-dashboard

---
+++ opentelemtry/Controllers/ClientController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace opentelemtry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // GET: api/<ClientController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInformation("return all Clients");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ClientController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id == 1)
+            {
+                _logger.LogWarning($"invalid Client model {ModelState.Count} {id} ", id);
+                return BadRequest();
+            }
+            _logger.LogInformation("return Client id {id}", id);
+
+            return Ok($"Client : {id}");
+        }
+
+        // POST api/<ClientController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
